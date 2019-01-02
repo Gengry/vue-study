07ab79e5ff4638502fd238858cb417d21ee17491
Geng <545620701@qq.com>
2019-01-02 15:55:34 +0800

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,39 @@
-module.exports = {
-  lintOnSave: false //语法检测
+const path = require('path')
+
+const resolve = dir => {
+  return path.join(__dirname, dir)
 }
+
+// 项目部署基础
+// 默认情况下，我们假设你的应用将被部署在域的根目录下,
+// 例如：https://www.my-app.com/
+// 默认：'/'
+// 如果您的应用程序部署在子路径中，则需要在这指定子路径
+// 例如：https://www.foobar.com/my-app/
+// 需要将它改为'/my-app/'
+// iview-admin线上演示打包路径： https://file.iviewui.com/admin-dist/
+const BASE_URL = process.env.NODE_ENV === 'production'
+  ? '/'
+  : '/'
+
+const BASE_URL = process.env.NODE_ENV === 'procution' ? '/cpw-admin' : '/'
+
+module.exports = {
+  baseUrl: BASE_URL,
+  // tweak internal webpack configuration.
+  // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+  // 如果你不需要使用eslint，把lintOnSave设为false即可
+  lintOnSave: true,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
+      .set('_c', resolve('src/components'))
+  },
+
+  productionSourceMap: false,
+  devServer: {
+    proxy: 'http://localhost:4000'
+  }
+
+}
+
